2e49f64587c73013aad66af7aa73e59f549e0e00
Johnicholas Hines <johnicholas.hines@gmail.com>
2015-01-23 19:33:22 -0200

    Checking the fix

--- learn_time/README.txt
+++ learn_time/README.txt
@@ -1,4 +1,4 @@
-This is what I see when I run ake:
+This is what I saw when I build and run (./ake clean; ./ake):
 
 	ceu learn_time.ceu
 	bash cc learn_time.c -o learn_time
@@ -6,7 +6,18 @@ This is what I see when I run ake:
 one
 dt 3000
 two
-dt 3000 <------------- I didn't expect to see this
+dt 3000 <-- This is what I wasn't expecting to see.
 three
 dt 5000
+four
+
+This is with the fix Dr. Sant'anna added:
+	ceu learn_time.ceu
+	bash cc learn_time.c -o learn_time
+	./learn_time
+one
+dt 3000ms
+two
+three
+dt 5000ms
 four
--- learn_time/learn_time.c
+++ learn_time/learn_time.c
@@ -1,11 +1,11 @@
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "_ceu_app.h"
-
 s32 WCLOCK_nxt = CEU_WCLOCK_INACTIVE;
 #define ceu_out_wclock_set(us) WCLOCK_nxt = us;
-
+bool reply_foo = 0;
 #include "_ceu_app.c"
 
 int main (int argc, char *argv[])
@@ -16,26 +16,16 @@ int main (int argc, char *argv[])
     app.data = (tceu_org*) &CEU_DATA;
     app.init = &ceu_app_init;
     app.init(&app);
-    // one
-    if (WCLOCK_nxt != CEU_WCLOCK_INACTIVE) {
-      s32 dt = WCLOCK_nxt;
-      printf("dt %d\n", dt / 1000);
-      ceu_sys_go(&app, CEU_IN__WCLOCK, &dt);
+    while (app.isAlive) {
+      if (WCLOCK_nxt != CEU_WCLOCK_INACTIVE) { 
+        s32 dt = WCLOCK_nxt;
+        printf("dt %dms\n", dt / 1000);
+        sleep(dt / (1000 * 1000));
+        ceu_sys_go(&app, CEU_IN__WCLOCK, &dt);
+      } else if (reply_foo) {
+        reply_foo = 0;
+        ceu_sys_go(&app, CEU_IN_FOO, NULL);
+      }
     }
-    // two
-    if (WCLOCK_nxt != CEU_WCLOCK_INACTIVE) {
-      s32 dt = WCLOCK_nxt;
-      printf("dt %d\n", dt / 1000);
-      ceu_sys_go(&app, CEU_IN__WCLOCK, &dt);
-    }
-    ceu_sys_go(&app, CEU_IN_FOO, NULL);
-    // three
-    if (WCLOCK_nxt != CEU_WCLOCK_INACTIVE) {
-      s32 dt = WCLOCK_nxt;
-      printf("dt %d\n", dt / 1000);
-      ceu_sys_go(&app, CEU_IN__WCLOCK, &dt);
-    }
-    // four
-    assert(!app.isAlive);
     return app.ret;
 }
