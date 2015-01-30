45838a8959bed930fab3dcc58b4f32c4e9fe6431
Johnicholas Hines <johnicholas.hines@gmail.com>
2015-01-30 19:40:11 -0200

    Try to get an array-based binary heap algorithm working based on Sean Barrett's stretchy buffers\n

---
+++ learn_stretchy_buffer/barngrader_example.ss
@@ -0,0 +1,3 @@
+c adapted from "Dijkstra Algorithm Example" by barngrader on YouTube: 0nVYi3o161A
+p aux sp ss 1
+s 1
---
+++ learn_stretchy_buffer/heap.c
@@ -0,0 +1,22 @@
+// Adapted from Rosetta Code
+#include <assert.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include "stretchy_buffer.h"
+
+#include "_ceu_app.h"
+#include "_ceu_app.c"
+
+int main(int argc, char* argv) {
+  byte CEU_DATA[sizeof(CEU_Main)];
+  memset(CEU_DATA, 0, sizeof(CEU_Main));
+  tceu_app app;
+  app.data = (tceu_org*) &CEU_DATA;
+  app.init = &ceu_app_init;
+  app.init(&app);
+  while (app.isAlive) {
+    ceu_sys_go(&app, CEU_IN_TICK, NULL);
+  }
+  return app.ret;
+}
+
