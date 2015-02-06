aa735986dcc332f3cd76fd015803c49b581d426a
Johnicholas Hines <johnicholas.hines@gmail.com>
2015-02-06 13:23:37 -0200

    Struggled with invalid scopes for a while, this seems to be working though.

--- learn_stretchy_buffer/heap.c
+++ learn_stretchy_buffer/heap.c
@@ -7,6 +7,47 @@
 #include "_ceu_app.h"
 #include "_ceu_app.c"
 
+void insert(heap* container, CEU_T* elem) {
+  int i = ++container->size;
+  if (container->size < sb_count(container->backing)) {
+    container->backing[container->size] = elem;
+  } else {
+    sb_push(container->backing, elem);
+  }
+  while (i > 1) {
+    int parent = i >> 1;
+    if (container->backing[i]->cost < container->backing[parent]->cost) {
+      CEU_T* temp = container->backing[i]; 
+      container->backing[i] = container->backing[parent];
+      container->backing[parent] = temp;
+    } else {
+      break;
+    }
+    i = parent;
+  }
+}
+
+void deletemin(heap* container) {
+  int i = 1;
+
+  container->backing[1] = container->backing[container->size];
+  container->size = container->size - 1;
+  int child = i << 1;
+  while (child <= container->size) {
+    if (child + 1 <= container->size && container->backing[child]->cost > container->backing[child+1]->cost) {
+      child = child + 1;
+    }
+    if (container->backing[child]->cost < container->backing[i]->cost) {
+      CEU_T* temp = container->backing[child];
+      container->backing[child] = container->backing[i];
+      container->backing[i] = temp;
+    } else {
+      break;
+    }
+    i = child;
+  }
+}
+
 int main(int argc, char* argv) {
   byte CEU_DATA[sizeof(CEU_Main)];
   memset(CEU_DATA, 0, sizeof(CEU_Main));
