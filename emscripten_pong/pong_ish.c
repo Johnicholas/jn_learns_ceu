768a41677553d376bab66138b97ee381656bef2c
Johnicholas Hines <johnicholas.hines@gmail.com>
2016-06-03 18:46:25 -0400

    Update pong_ish.c

--- emscripten_pong/pong_ish.c
+++ emscripten_pong/pong_ish.c
@@ -9,6 +9,8 @@ TTF_Font* font;
 
 #include "ceu_types.h"
 
+s32 WCLOCK_nxt;
+#define ceu_out_wclock_set(us) WCLOCK_nxt = us;
 #define ceu_out_assert(X) assert(X)
 #define ceu_out_log(X) printf("%s\n", X)
 
@@ -19,26 +21,41 @@ static tceu_app app;
 
 void one_iter() {
   SDL_Event event;
-  int mx, my;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
       case SDL_MOUSEMOTION: {
         SDL_MouseMotionEvent* m = (SDL_MouseMotionEvent*)&event;
-        SDL_GetMouseState(&mx, &my);
-        ceu_sys_go(&app, CEU_IN_MOUSEMOTION, 0);
+        tceu__int__int payload;
+        SDL_GetMouseState(&payload._1, &payload._2);
+        ceu_sys_go(&app, CEU_IN_SDL_MOUSEMOTION, &payload);
         break;
       }
     }
   }
-  s32 dt_us = 16000;
-  ceu_sys_go(&app, CEU_IN__WCLOCK, &dt_us);
+
+  if (WCLOCK_nxt != CEU_WCLOCK_INACTIVE) {
+    s32 dt_us = 16000;
+    ceu_sys_go(&app, CEU_IN__WCLOCK, &dt_us);
+    while (WCLOCK_nxt <= 0) {
+      dt_us = 0;
+      ceu_sys_go(&app, CEU_IN__WCLOCK, &dt_us);
+    }
+  }
+
+  ceu_sys_go(&app, CEU_IN_SDL_REDRAW, 0);
 }
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
-  screen = SDL_SetVideoMode(W, H, 32, SDL_SWSURFACE);
+  WCLOCK_nxt = CEU_WCLOCK_INACTIVE;
+  screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
   TTF_Init();
   font = TTF_OpenFont("sans-serif", 40);
+
+  memset(CEU_DATA, 0, sizeof(CEU_Main));
+  app.data = (tceu_org*) &CEU_DATA;
+  app.init = &ceu_app_init;
+  app.init(&app);
 
   // void emscripten_set_main_loop(callback, fps, simulate_infinite_loop)
   emscripten_set_main_loop(one_iter, 0, 0);
